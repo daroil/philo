ccaaa5ed6fc47811d1938b43bf440862b09d4e7b
Danila <gendel2403@gmail.com>
2023-03-03 19:29:28 +0100

    playing with threads

--- main.c
+++ main.c
@@ -6,23 +6,59 @@
 /*   By: dhendzel <dhendzel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/26 16:58:09 by dhendzel          #+#    #+#             */
-/*   Updated: 2023/02/26 17:03:34 by dhendzel         ###   ########.fr       */
+/*   Updated: 2023/03/03 19:19:35 by dhendzel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int	main(int argc, char **argv)
-{
-	int	i;
 
-	if(argc && argv)
-		printf("Succesful start\n");
-	i = 0;
-	while (argv[i])
-	{
-		printf("input %s\n", argv[i]);
-		i++;
-	}
-	return (0);	
+
+// Global variable:
+
+void* foo(void* p)
+{
+	t_philo *philo;
+
+	philo = (t_philo *) p;
+	// Print value received as argument:
+	printf("Value recevied as argument in starting routine: ");
+	printf("%i\n", philo->input);
+
+	// Return reference to global variable:
+	pthread_exit((void *)&philo->output);
 }
+
+int main(void)
+{
+	// int i = 2;
+	t_philo philo;
+  // Declare variable for thread's ID:
+	pthread_t id;
+	philo.input = 1;
+	philo.output = 2;
+	// int j = 1;
+  	int* ptr;
+	
+	pthread_create(&id, NULL, foo, &philo);
+    // Wait for foo() and retrieve value in ptr;
+	pthread_join(id, (void**)&ptr);
+	printf("Value recevied by parent from child: ");
+	printf("%i\n", *ptr);
+}
+
+
+// int	main(int argc, char **argv)
+// {
+// 	int	i;
+
+// 	if (argc && argv)
+// 		printf("Succesful start\n");
+// 	i = 0;
+// 	while (argv[i])
+// 	{
+// 		printf("input %s\n", argv[i]);
+// 		i++;
+// 	}
+// 	return (0);	
+// }
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: dhendzel <dhendzel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/26 16:58:40 by dhendzel          #+#    #+#             */
-/*   Updated: 2023/02/26 17:01:59 by dhendzel         ###   ########.fr       */
+/*   Updated: 2023/03/03 19:14:55 by dhendzel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,14 @@
 # define PHILO_H
 
 #include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+
+typedef struct philo_s
+{
+	int		input;
+	int		output;
+}	t_philo;
 
 
 #endif
